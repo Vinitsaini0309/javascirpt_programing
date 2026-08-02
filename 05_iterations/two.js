c2feda0f5bc06f567a3c98630f564ae7cc162abf
Vinit saini <vinitmali0309@gmail.com>
2026-08-02 11:17:58 +0000

    commit 28

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+//  while
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 3
+}
+
+let myarray = ['vinit' , 'rajveer' , 'Omprakash']
+let arr = 0
+while (arr < myarray.length) {
+    // console.log(`Name of ideal is ${myarray[arr]}`);
+    arr++
+    
+}
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <=10);
